82168d2a101dc5ad80d2380fff0cc9599f96782f
Alexy Auffret <alexyauffre@gmail.com>
2021-04-12 10:02:29 +0200

    init commit

---
+++ src/main/java/fr/cesi/myfreelanceenterpriseback/MyfreelanceenterprisebackApplication.java
@@ -0,0 +1,13 @@
+package fr.cesi.myfreelanceenterpriseback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfreelanceenterprisebackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfreelanceenterprisebackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/cesi/myfreelanceenterpriseback/MyfreelanceenterprisebackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cesi.myfreelanceenterpriseback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfreelanceenterprisebackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
