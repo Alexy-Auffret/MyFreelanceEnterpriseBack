75107f4632530b227b6013008e79e72fc3451086
Thibault MAUDET <41793338+thibaultmaudet@users.noreply.github.com>
2021-04-15 09:09:01 +0200

    Ajout HomeController

--- src/main/java/fr/cesi/myFreelanceEntreprise/controller/HomeController.java
+++ src/main/java/fr/cesi/myFreelanceEntreprise/controller/HomeController.java
@@ -1,26 +1,31 @@
 package fr.cesi.myFreelanceEntreprise.controller;
 
+import fr.cesi.myFreelanceEntreprise.beans.Bill;
 import fr.cesi.myFreelanceEntreprise.service.BillService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
-@Controller
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
     @Autowired
     BillService billService;
 
-
-    //ToDo : Gérer les informations de la page d’accueil du site.
-
-
-    // ToDo : 1. La barre de progression représente le chiffre d’affaires hors taxe encaissé cette année. Un auto-entrepreneur ne peux encaisser plus de 70000 euros par an sans changer de statut.
-
-
-    //ToDo : 1. Lorsqu’une facture est payée, elle vient augmenter la barre de progression sur la page d’accueil du site.
-
+    @RequestMapping(value = "/getMonthBills", method = RequestMethod.GET)
+    public List<Bill> getMonthBills() {
+        return billService.getMonthBills();
+    }
+    
+    @RequestMapping(value = "/getRevenues", method = RequestMethod.GET)
+    public float getRevenues() {
+        return billService.getRevenues();
+    }
 
     //ToDo : 5. Un graphique me permet de voir, mois après mois, le chiffre d’affaires réalisé.
-
 
     //ToDo : 6. L’application m’indique le montant des charges URSAFF chaque trimestre (22,2%) et le montant de la TVA. Je peux ainsi savoir, le montant exact de ma rémunération mensuelle.
 }
--- src/main/java/fr/cesi/myFreelanceEntreprise/service/BillService.java
+++ src/main/java/fr/cesi/myFreelanceEntreprise/service/BillService.java
@@ -5,9 +5,13 @@ import fr.cesi.myFreelanceEntreprise.beans.Step;
 import fr.cesi.myFreelanceEntreprise.dao.BillDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class BillService {
     @Autowired
@@ -30,4 +34,22 @@ public class BillService {
 
     public void save(Bill b) { billDAO.save(b);}
 
+    public List<Bill> getMonthBills() {
+        LocalDate firstDayInMonth = LocalDate.now().withDayOfMonth(1);
+
+        List<Bill> bills = billDAO.findAll();
+
+        return bills.stream().filter(x -> x.getCreationDate().after(Date.from(firstDayInMonth.atStartOfDay(ZoneId.systemDefault()).toInstant()))).collect(Collectors.toList());
+    }
+
+    public float getRevenues() {
+        List<Bill> bills = getMonthBills();
+
+        float revenues = 0;
+
+        for (Bill bill:bills)
+            revenues += bill.getAmount();
+
+        return revenues;
+    }
 }
