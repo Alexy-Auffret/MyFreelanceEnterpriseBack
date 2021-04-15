84b2451ce1f7bd93f0da965f132548460cec2c31
Alexy Auffret <alexyauffre@gmail.com>
2021-04-15 17:37:39 +0200

    update BillBean

--- src/main/java/fr/cesi/application/myFreelanceEntreprise/controller/ClientController.java
+++ src/main/java/fr/cesi/application/myFreelanceEntreprise/controller/ClientController.java
@@ -1,13 +1,9 @@
 package fr.cesi.application.myFreelanceEntreprise.controller;
 
-import fr.cesi.application.myFreelanceEntreprise.beans.Bill;
 import fr.cesi.application.myFreelanceEntreprise.beans.Client;
 import fr.cesi.application.myFreelanceEntreprise.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -43,7 +39,6 @@ public class ClientController {
         c.setPhoneNumber(phoneNumber);
         c.setActive(active);
         clientService.save(c);
-
         return "Le client " + c.getName() + " a bien été modifié.";
     }
 }
